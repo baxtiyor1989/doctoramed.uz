5fa259c9f2ebc05c6b26b602d6319f506d4d1484
Baxtiyor Yuldashev <baxtiyor1989@gmail.com>
2026-07-13 16:34:38 +0500

    130720261413

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -112,8 +112,7 @@
                         <div class="dropdown ms-sm-3 header-item topbar-user">
                             <button type="button" class="btn" data-bs-toggle="dropdown">
                                 <span class="d-flex align-items-center">
-                                    <img class="rounded-circle header-profile-user" src="{{ asset('admin-assets/assets/images/users/avatar-1.jpg') }}" alt="Admin">
-                                    <span class="text-start ms-xl-2">
+                                    <span class="text-start">
                                         <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{ auth()->user()->name }}</span>
                                         <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Administrator</span>
                                     </span>
